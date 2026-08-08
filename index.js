edcefb868e463e2edca8caf122d5b0b47bf37638
candyvicky072 <candyvicky072@gmail.com>
2026-08-08 13:56:36 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const TelegramBot = require("node-telegram-bot-api");
+const Parser = require("rss-parser");
+const parser = new Parser();
 
 const token = process.env.BOT_TOKEN;
 const bot = new TelegramBot(token, { polling: true });
